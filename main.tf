d0da98773b0eecdbd1103b175460b560fccece11
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-06-18 19:33:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "ramesh2805-terraform-demo-bucket-2026"
+  bucket = "ramesh2805-terraform-demo-bucket-June-2026"
   acl    = "private"
 
   control_object_ownership = true
